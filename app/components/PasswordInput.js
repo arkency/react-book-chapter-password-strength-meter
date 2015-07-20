8da579ce6cbb0e3a9caa86fbcf8f724daafb5763
Marcin Grzywaczewski <killavus@gmail.com>
2015-07-20 18:16:00 +0200

    Implemented the basic grid (based on HTML mockup)

--- app/components/PasswordInput.js
+++ app/components/PasswordInput.js
@@ -1,8 +1,28 @@
 import React from 'react/addons';
+import { Grid, Row, Col } from 'react-bootstrap';
 
 class PasswordInput extends React.Component {
   render() {
-    return null;
+    return (
+            <Grid>
+             <Row>
+               <PasswordField />
+               <StrengthMeter />
+             </Row>
+            </Grid>
+           );
+  }
+}
+
+class PasswordField extends React.Component {
+  render() {
+    return (<Col md={8} />);
+  }
+}
+
+class StrengthMeter extends React.Component {
+  render() {
+    return (<Col md={4} />);
   }
 }
 
--- app/index.js
+++ app/index.js
@@ -1,7 +1,8 @@
 import '../node_modules/bootstrap/dist/css/bootstrap.css';
 import React from 'react';
+import PasswordInput from './components/PasswordInput';
 
 const APP_ROOT = document.createElement("div");
 document.body.appendChild(APP_ROOT);
 
-React.render(<p>Hello!</p>, APP_ROOT);
+React.render(<PasswordInput />, APP_ROOT);
