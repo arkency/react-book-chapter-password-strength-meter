8f5949f48c9de24e9d5f5c0916bab4e5987ca955
Marcin Grzywaczewski <killavus@gmail.com>
2015-07-20 18:34:39 +0200

    Added good password principles via default properties

--- app/components/PasswordInput.js
+++ app/components/PasswordInput.js
@@ -14,6 +14,20 @@ class PasswordInput extends React.Component {
   }
 }
 
+const SPECIAL_CHARS_REGEX = /[^A-Za-z0-9]/;
+const DIGIT_REGEX = /[0-9]/;
+
+PasswordInput.defaultProps = {
+  goodPasswordPrinciples: [
+    { label: "6+ characters",
+      predicate: (input) => input.length > 6 },
+    { label: "with at least one digit",
+      predicate: (input) => input.match(DIGIT_REGEX) !== null },
+    { label: "with at least one special character",
+      predicate: (input) => input.match(SPECIAL_CHARS_REGEX) !== null }
+  ]
+};
+
 class PasswordField extends React.Component {
   render() {
     return (<Col md={8} />);
